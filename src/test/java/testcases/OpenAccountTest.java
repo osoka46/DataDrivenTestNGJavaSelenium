d091875ce3473160e6c11e1b7b5b36450069802f
Obzdag <osman.bozdag@yahoo.com>
2023-06-21 17:13:31 +0200

    .

---
+++ src/test/java/testcases/OpenAccountTest.java
@@ -0,0 +1,48 @@
+package testcases;
+
+import base.TestBase;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.openqa.selenium.Alert;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class OpenAccountTest extends TestBase {
+
+    @Parameters({"firstName", "lastName", "postCode", "confirmation"})
+    @Test()
+    public void openAccount(String firstName, String lastName, String postCode, String confirmation) {
+
+     /*   String firstName = "osman";
+        String lastName = "bzd";
+        String postCode = "123";
+        String confirmation = "Customer added successfully";*/
+        Alert alert;
+
+
+        clickElement(driver, "addCustomerBtn_CSS");
+        typeText(driver, "firstName_CSS", firstName);
+        typeText(driver, "lastName_CSS", lastName);
+        typeText(driver, "postCode_CSS", postCode);
+        clickElement(driver, "addBtn_CSS");
+
+
+        /*driver.findElement(By.cssSelector(or.getProperty("addCustomerBtn_CSS"))).click();
+        driver.findElement(By.cssSelector(or.getProperty("firstName_CSS"))).sendKeys(firstName);
+        driver.findElement(By.cssSelector(or.getProperty("lastName_CSS"))).sendKeys(lastName);
+        driver.findElement(By.cssSelector(or.getProperty("postCode_CSS"))).sendKeys(postCode);
+        driver.findElement(By.cssSelector(or.getProperty("addBtn_CSS"))).click();*/
+
+        expectedWait(driver, 5);
+        alert = driver.switchTo().alert();
+        String confirm = alert.getText();
+        alert.accept();
+        Assert.assertTrue(confirm.contains(confirmation));
+
+    }
+
+
+
+}
